60d7c9a8b5acd8daa6249e8ab8633743075c1c66
cmoreau004 <chloe.moreau37@hotmail.fr>
2018-08-10 10:44:00 +0200

    Delete unused method

--- src/test/java/com/cmoreau/springbootreactorretrofit/GitHubSamples.java
+++ src/test/java/com/cmoreau/springbootreactorretrofit/GitHubSamples.java
@@ -17,9 +17,5 @@ public class GitHubSamples {
         Stats stats = new Stats<>(TOTAL_FOLLOWERS_NB, TOTAL_REPOS_NB);
         return stats.toString();
     }
-
-    static String getMembers(){
-        Flux<String> flux = Flux.just(USER_LOGIN1 + "(" + USER_NAME1+")", USER_LOGIN2 + "(" + USER_NAME2+")");
-        return flux.toString();
-    }
+    
 }
